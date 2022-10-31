b9d21b2f49febaee8d08c394cab611d5794763ff
cellmer <michal.cellmer@gmail.com>
2022-10-31 12:07:25 +0100

    device_number: device_number.c: change functions names

--- device_number/device_number.c
+++ device_number/device_number.c
@@ -28,14 +28,14 @@ static struct file_operations fops = {
 };
 
 // function called when the module is loaded into the kernel
-static int __init hello_init(void)
+static int __init driver_init(void)
 {
     printk(KERN_ALERT "Hello world!\n");
     return 0;
 }
 
 // function called when the module is removed from the kernel
-static void __exit hello_exit(void)
+static void __exit driver_exit(void)
 {
     printk(KERN_ALERT "Goodbye, cruel world\n");
 }
