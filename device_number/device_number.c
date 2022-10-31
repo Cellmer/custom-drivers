46e0cc88b317391d92317f28e9a56555a2e02424
cellmer <michal.cellmer@gmail.com>
2022-10-31 12:19:38 +0100

    device_number: device_number.c: add registering and unregistering device number

--- device_number/device_number.c
+++ device_number/device_number.c
@@ -2,6 +2,8 @@
 #include <linux/module.h>
 #include <linux/fs.h>
 
+#define MYMAJOR 90
+
 // meta data
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Michal Cellmer");
@@ -10,12 +12,12 @@ MODULE_DESCRIPTION("Register device number and create simple callback functions"
 // function called when the device file is opened
 static int driver_open(struct inode *device_file, struct file *instance)
 {
-    printk("device_number - open was called\n")
+    printk("device_number - open was called\n");
     return 0;
 }
 
 // function called when the device file is closed
-static int driver_close(struct indoe *device_file, struct file *instance)
+static int driver_close(struct inode *device_file, struct file *instance)
 {
     printk("device_number - close was called\n");
     return 0;
@@ -30,15 +32,33 @@ static struct file_operations fops = {
 // function called when the module is loaded into the kernel
 static int __init driver_init(void)
 {
-    printk(KERN_ALERT "Hello world!\n");
+    int retval;
+    printk("Hello Kernel!\n");
+
+    // register device number
+    retval = register_chrdev(MYMAJOR, "my_dev_nr", &fops);
+    if(retval == 0)
+    {
+        printk("device_numer - registered device number Major: %d, Minor: %d\n", MYMAJOR, 0);
+    }
+    else if(retval > 0)
+    {
+        printk("device_number - registered device number Major: %d, Minor: %d\n", retval>>20, retval&0xfffff);
+    }
+    else
+    {
+        printk("Could not register device number!\n");
+        return -1;
+    }
     return 0;
 }
 
 // function called when the module is removed from the kernel
 static void __exit driver_exit(void)
 {
-    printk(KERN_ALERT "Goodbye, cruel world\n");
+    unregister_chrdev(MYMAJOR, "my_dev_nr");
+    printk(KERN_ALERT "Goodbye Kernel!\n");
 }
 
-module_init(hello_init);
-module_exit(hello_exit);
+module_init(driver_init);
+module_exit(driver_exit);
