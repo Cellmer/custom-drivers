85413aa023a732f1332a1d2b75332ae708d4bf84
cellmer <michal.cellmer@gmail.com>
2022-10-31 11:59:03 +0100

    device_number: add Makefile and create a simple driver template

---
+++ device_number/device_number.c
@@ -0,0 +1,24 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/fs.h>
+
+// meta data
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Michal Cellmer");
+MODULE_DESCRIPTION("Register device number and create simple callback functions");
+
+// function called when the module is loaded into the kernel
+static int __init hello_init(void)
+{
+    printk(KERN_ALERT "Hello world!\n");
+    return 0;
+}
+
+// function called when the module is removed from the kernel
+static void __exit hello_exit(void)
+{
+    printk(KERN_ALERT "Goodbye, cruel world\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
