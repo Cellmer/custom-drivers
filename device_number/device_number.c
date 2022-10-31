8b2421d5c9404447805f1aa1ce4c2c1e87678e08
cellmer <michal.cellmer@gmail.com>
2022-10-31 12:05:57 +0100

    device_number: device_number.c: add callback functions and file_operations structure

--- device_number/device_number.c
+++ device_number/device_number.c
@@ -7,6 +7,26 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Michal Cellmer");
 MODULE_DESCRIPTION("Register device number and create simple callback functions");
 
+// function called when the device file is opened
+static int driver_open(struct inode *device_file, struct file *instance)
+{
+    printk("device_number - open was called\n")
+    return 0;
+}
+
+// function called when the device file is closed
+static int driver_close(struct indoe *device_file, struct file *instance)
+{
+    printk("device_number - close was called\n");
+    return 0;
+}
+
+static struct file_operations fops = {
+    .owner = THIS_MODULE,
+    .open = driver_open,
+    .release = driver_close
+};
+
 // function called when the module is loaded into the kernel
 static int __init hello_init(void)
 {
