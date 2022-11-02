54d9cbb4caa6b428d98a4c0831a501d0698d5ad8
cellmer <michal.cellmer@gmail.com>
2022-11-02 08:07:04 +0100

    read_write: read_write.c: add read and write callback functions

--- read_write/read_write.c
+++ read_write/read_write.c
@@ -1,13 +1,51 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/uaccess.h>
 
 #define MYMAJOR 90
+#define BUFFER_SIZE 255
 
 // meta data
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Michal Cellmer");
 MODULE_DESCRIPTION("Register device number and create simple callback functions");
+
+// buffer for data
+static char buffer[BUFFER_SIZE];
+static int buffer_pointer;
+
+// function called when user want to read from the device file
+static ssize_t driver_read(struct file *File, char *user_buffer, size_t count, loff_t *offs)
+{
+    int to_copy, not_copied, delta;
+
+    to_copy = min(count, buffer_pointer);
+
+    // copy data to user
+    not_copied = copy_to_user(user_buffer, buffer, to_copy);
+
+    delta = to_copy - not_copied;
+
+    return delta;
+}
+
+// function called when user want to write to the device file
+static ssize_t driver_write(struct file *File, char *user_buffer, size_t count, loff_t *offs)
+{
+    int to_copy, not_copied, delta;
+
+    to_copy = min(count, sizeof(buffer));
+
+    // copy data from user
+    not_copied = copy_from_user(buffer, user_buffer, to_copy);
+    buffer_pointer = to_copy;
+
+    delta = to_copy - not_copied;
+
+    return delta;
+}
 
 // function called when the device file is opened
 static int driver_open(struct inode *device_file, struct file *instance)
@@ -26,7 +64,9 @@ static int driver_close(struct inode *device_file, struct file *instance)
 static struct file_operations fops = {
     .owner = THIS_MODULE,
     .open = driver_open,
-    .release = driver_close
+    .release = driver_close,
+    .read = driver_read,
+    .write = driver_write
 };
 
 // function called when the module is loaded into the kernel
